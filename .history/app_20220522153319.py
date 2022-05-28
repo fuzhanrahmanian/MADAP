d2e680c19f6ab98ea58662964e3f182178b789dd
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-05-28 15:33:37 +0200

    never gonna run around and desert you haha

--- .history/app_20220522153319.py
+++
@@ -1,7 +0,0 @@
-import logger
-log = logger.setup_applevel_logger(file_name = 'app_debug.log')
-import mymodule
-log.debug('Calling module function.')
-
-mymodule.multiply(5, 2)
-log.debug('Finished.')
