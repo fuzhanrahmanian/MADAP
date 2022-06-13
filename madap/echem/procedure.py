e6c4bce4c8d6f26c2eb3a3b45123631b72cee6c7
fuzhanr <fuzhanrahmanian@gmail.com>
2022-06-13 23:08:00 +0200

    set an abstract class for consistency

--- madap/echem/procedure.py
+++ madap/echem/procedure.py
@@ -2,11 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class EChemProcedure(ABC):
-    
+
     @abstractmethod
     def analyse():
         pass
-    
+
     @abstractmethod
     def plot():
         pass
