d3c372d6e5280b50bdc92b089b373ab4952d7268
fuzhanr <fuzhanrahmanian@gmail.com>
2022-06-13 13:04:24 +0200

    add to read more file extenstions and check the validity of data type

--- madap/data_acquisition/data_acquisition.py
+++ madap/data_acquisition/data_acquisition.py
@@ -1,9 +1,10 @@
 import os
 from madap import logger
 import pandas as pd
+import numpy as np
 
 log = logger.get_logger("data_acquisition")
-EXTENSIONS = [".txt", ".csv", ".json", ".xlsx", ".zip", ".hdf"]
+EXTENSIONS = [".txt", ".csv", ".json", ".xlsx", ".hdf5", ".h5", ".pkl"]
 
 def acquire_data(data_path):
     """Acquire data from a given file
@@ -16,17 +17,54 @@ def acquire_data(data_path):
     """
     _ , extension = os.path.splitext(data_path)
 
+    log.info(f"Importing {extension} file.")
+
     if extension not in EXTENSIONS:
         log.error(f"Datatype not supported. Supported datatypes are: {EXTENSIONS}")
         raise ValueError("Datatype not supported")
 
-    log.info(f"Importing {extension} file.")
-    if extension == ".csv" or extension ==".txt":
+    elif extension == ".csv" or extension ==".txt":
         df = pd.read_csv(data_path, sep=None, engine="python")
     elif extension == ".xlsx":
         df = pd.read_excel(data_path)
     elif extension == ".json":
         df = pd.read_json(data_path)
+    elif extension == ".hdf5" or extension == ".h5":
+        df = pd.read_hdf(data_path)
+    elif extension == ".pkl":
+        df = pd.read_pickle(data_path)
 
     return df
+
+
+def format_data(data):
+    """Convert the given data to the array of float
+
+    Args:
+        data (_type_): Given data
+
+    Returns:
+        A readable format of data for analysis
+    """
+
+    if not np.array_equal(data, data.astype(float)):
+        data = data.astype(np.float)
+
+    return data
+
+def calculate_phaseshift(imaginary_impedance, real_impedance):
+    """calculate phase shift
+
+    Args:
+        imaginary_impedance (class): imaginary impedance data
+        real_impedance (class): real impedance data
+
+    Returns:
+        phase shift: calculated phase shift based on real and imaginary data
+    """
+
+    return np.arctan(format_data(imaginary_impedance)/format_data(real_impedance))
+
+
+
 
