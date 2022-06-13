b9f15f7b63cb069cbd212a61737df8dfdd9e5d22
fuzhanr <fuzhanrahmanian@gmail.com>
2022-06-13 13:03:42 +0200

    ability to read unicode character

--- madap/logger/logger.py
+++ madap/logger/logger.py
@@ -8,7 +8,7 @@ def setup_applevel_logger(logger_name = APP_LOGGER_NAME, file_name=None):
 
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter(u"%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     streamHandler = logging.StreamHandler(sys.stdout)
     streamHandler.setFormatter(formatter)
     logger.handlers.clear()
@@ -16,7 +16,7 @@ def setup_applevel_logger(logger_name = APP_LOGGER_NAME, file_name=None):
     if file_name:
         if not os.path.exists("logs"):
             os.makedirs("logs")
-        fh = logging.FileHandler(f"./logs/{file_name}")
+        fh = logging.FileHandler(f"./logs/{file_name}", encoding="utf-8")
         fh.setFormatter(formatter)
         logger.addHandler(fh)
     return logger
