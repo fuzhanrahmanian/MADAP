c5a819bfb038fc193ce5e5c8ca100597d7b34516
fuzhanr <fuzhanrahmanian@gmail.com>
2022-06-15 15:52:38 +0200

    automatic or manual  eis fitting

--- madap/echem/impedance.py
+++ madap/echem/impedance.py
@@ -1,31 +1,99 @@
+from dataclasses import dataclass, field
+from tkinter import E
+from impedance import preprocessing
+from impedance.models.circuits import CustomCircuit, fitting
+from impedance.validation import linKK
+import numpy as np
+import json
 from echem.procedure import EChemProcedure
+from madap import logger
+import scipy.stats as ss
+#from madap import plotting
 
+log = logger.get_logger("impedance")
+
+# pylint: disable=unsubscriptable-object
+@dataclass(frozen=True, order=True)
 class EIS(EChemProcedure):
-    def __init__(self, data, freq_idx, real_idx, imag_idx, phase_shift = None) -> None:
-        
-        self.frequency = data[freq_idx]
-        self.real_impedance = data[real_idx]
-        self.imaginary_impedance = data[imag_idx]
-        self.phase_shift = phase_shift 
-        
-    def analyse():
+    frequency : list[float] = field(default_factory=list) #= format_data(data[freq_idx])
+    real_impedance : list[float] = field(default_factory=list)
+    imaginary_impedance : list[float] = field(default_factory=list)
+    phase_shift : list[float] = field(default_factory=list)
+
+
+    # Schönleber, M. et al. A Method for Improving the Robustness of linear Kramers-Kronig Validity Tests.
+    # Electrochimica Acta 131, 20–27 (2014) doi: 10.1016/j.electacta.2014.01.034.
+    def fit_eis(self, suggested_circuit: str = None, initial_value=None, max_rc_element: int=20, cut_off: float=0.85,
+                fit_type: str='complex', val_low_freq=True, save_dir= None):
+        f, Z = np.array(self.frequency), np.array(self.real_impedance + 1j*self.imaginary_impedance)
+
+        num_rc, eval_fit , Z_linKK, res_real, res_imag = linKK(f, Z, c=cut_off, max_M=max_rc_element, fit_type=fit_type, add_cap=val_low_freq)
+        chi_val = self.chi_calculation(res_imag, res_real)
+        log.info(f"Chi value from lin_KK method is {chi_val}")
+
+        if any(x < 0 for x in self.imaginary_impedance):
+            f, Z = preprocessing.ignoreBelowX(f, Z)
+
+        # if the user did not choose any circuit, some default suggestions will be applied.
+        if (suggested_circuit and initial_value) is None:
+            with open("suggested_circuits.json", "r") as file:
+                suggested_circuits = json.load(file)
+
+            # Random default threshold for rmse
+            rmse_error = 100
+            for guess_circuit, guess_value in suggested_circuits.items():
+                # apply some random guess
+                customCircuit_guess = CustomCircuit(initial_guess=guess_value, circuit=guess_circuit)
+
+                try:
+                    customCircuit_guess.fit(f, Z)
+
+                except RuntimeError as e:
+                    log.error(e)
+                    continue
+
+                Z_fit_guess = customCircuit_guess.predict(f)
+                rmse_guess = fitting.rmse(Z, Z_fit_guess)
+                log.info(f"With the guessed circuit {guess_circuit} the RMSE error is {rmse_guess}")
+
+                if rmse_guess < rmse_error:
+                    rmse_error = rmse_guess
+                    customCircuit = customCircuit_guess
+                    Z_fit = Z_fit_guess
+
+        else:
+                customCircuit = CustomCircuit(initial_guess=initial_value, circuit=suggested_circuit)
+                customCircuit.fit(f, Z)
+                Z_fit = customCircuit.predict(f)
+                rmse_error = fitting.rmse(Z, Z_fit)
+
+        # plot
+
+        if save_dir:
+            # save the data
+            # save the plot
+            customCircuit.save(r"\Repositories\MADAP\test.json")
+
+        return None
+
+    def chi_calculation(self, res_imag, res_real):
+        return np.sum(np.square(res_imag) + np.square(res_real))
+
+    def rmse_plausibility(self, rmse_error):
+        if not rmse_error < 100:
+            log.warning(f"A validated circuit was not found with having a fit error of {rmse_error}")
+
+
+    def analyse(self):
         pass
-    def plot():
+    def plot(self):
         pass
 
 
-
-# stuff that should go her
-
-
-# todo: make an abstract class out of this
-# class EIS_A(plot, data): # plot and data of our defined classes maybe open a folder calls "util" and we can call it from there. There data will be assembled, new directories can be created, dataframe can be built.
+# class EIS_A(plot, data): # plot and data of our defined classes maybe open a folder calls "util" and we can call it from there.
+# There data will be assembled, new directories can be created, dataframe can be built.
 #                        # from plot we will import the plot styles, colots, type, subplots, colot bar , legend ... maybe learn the kwargs**
-#     def __init__(self, frequency, real_impedance, imaginary_impedance) -> None:
-#         self.frequency = frequency
-#         self.real_impedance = real_impedance
-#         self.imaginary_impedance = imaginary_impedance
-#         self.phase_shift => optional
+
 #         # logging info to show the what we gave
 #         def nyquist_plot():pass
 #         def bode_plot(): pass
@@ -36,3 +104,4 @@ class EIS(EChemProcedure):
 #         def plot_residual(): pass # should be part of validity
 #         def llissajous_plot(): pass # show the linearity V-I , v vs time and i vs time , stability , and casuality and stability
 #         def analsys_eis(): pass # all the functionions except lissajous
+ 
