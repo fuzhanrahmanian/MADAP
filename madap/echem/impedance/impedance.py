bae9a272995efbf07744af21a9d7e41b35af87e6
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-06-19 21:41:05 +0200

    composite class format

--- madap/echem/impedance/impedance.py
+++ madap/echem/impedance/impedance.py
@@ -19,39 +19,25 @@ log = logger.get_logger("impedance")
 
 # pylint: disable=unsubscriptable-object
 @define
-class Impedance(EChemProcedure):
+class Impedance:
     frequency : list[float] = field(on_setattr=frozen)
     real_impedance : list[float] = field(on_setattr=frozen)
     imaginary_impedance : list[float] = field( on_setattr=frozen)
     phase_shift : list[float] = field(on_setattr=frozen)
-    voltage : float = None
-    chi_val: float = None
-    Z_fit: list[float] = None
-    custom_circuit: str = None
-    res_real : list[float] = None
-    res_imag : list[float] = None
 
-    def analyze():
-        pass
 
-    def plot():
-        pass
 
-    def save_data():
-        pass
-
-    def perform_all_actions():
-        pass
 #class EIS(Impedance):
-class EIS:
+class EIS(EChemProcedure):
     # def __init__(self, frequency, real_impedance, imaginary_impedance, phase_shift, voltage,
     #             suggested_circuit: str = None, initial_value=None, max_rc_element: int=20,
     #             cut_off: float=0.85, fit_type: str='complex', val_low_freq=True) -> None:
-    def __init__(self, impedance, suggested_circuit: str = None, initial_value=None, max_rc_element: int=20,
-                cut_off: float=0.85, fit_type: str='complex', val_low_freq=True):
+    def __init__(self, impedance, voltage: float = None, suggested_circuit: str = None, initial_value = None, max_rc_element: int = 20,
+                cut_off: float = 0.85, fit_type: str = 'complex', val_low_freq: bool = True):
     #             cut_off: float=0.85, fit_type: str='complex', val_low_freq=True)
         # super().__init__(frequency, real_impedance, imaginary_impedance, phase_shift, voltage)
         self.impedance = impedance
+        self.voltage = voltage
         self.suggested_circuit = suggested_circuit
         self.initial_value = initial_value
         self.max_rc_element = max_rc_element
@@ -59,18 +45,17 @@ class EIS:
         self.fit_type = fit_type
         self.val_low_freq = val_low_freq
 
-
     # Schönleber, M. et al. A Method for Improving the Robustness of linear Kramers-Kronig Validity Tests.
     # Electrochimica Acta 131, 20–27 (2014) doi: 10.1016/j.electacta.2014.01.034.
     def analyze(self):
-        f, Z = np.array(self.frequency), np.array(self.real_impedance + 1j*self.imaginary_impedance)
+        f, Z = np.array(self.impedance.frequency), np.array(self.impedance.real_impedance + 1j*self.impedance.imaginary_impedance)
 
-        num_rc, eval_fit , Z_linKK, self.res_real, self.res_imag = linKK(f, Z, c=self.cut_off, max_M=self.max_rc_element,
-        fit_type=self.fit_type, add_cap=self.val_low_freq)
+        self.num_rc_linKK, self.eval_fit_linKK , self.Z_linKK, self.res_real, self.res_imag = linKK(f, Z, c=self.cut_off, max_M=self.max_rc_element,
+                                                                        fit_type=self.fit_type, add_cap=self.val_low_freq)
         self.chi_val = self._chi_calculation(self.res_imag, self.res_real)
         log.info(f"Chi value from lin_KK method is {self.chi_val}")
 
-        if any(x < 0 for x in self.imaginary_impedance):
+        if any(x < 0 for x in self.impedance.imaginary_impedance):
             f, Z = preprocessing.ignoreBelowX(f, Z)
 
         # if the user did not choose any circuit, some default suggestions will be applied.
@@ -108,7 +93,7 @@ class EIS:
 
     def plot(self, save_dir, plots):
 
-        plot_dir=utils.create_dir(os.path.join(save_dir, "plots"))
+        plot_dir = utils.create_dir(os.path.join(save_dir, "plots"))
         plot = iplt()
         if len(plots)%2==0:
             num_row = num_column = len(plots)/2
@@ -119,11 +104,11 @@ class EIS:
 
         for i, plot_name in enumerate(plots):
             if plot_name =="nyquist":
-                plot.nyquist(ax=ax, frequency=self.frequency, real_impedance=self.real_impedance, imaginary_impedance=self.imaginary_impedance,
+                plot.nyquist(ax=ax, frequency=self.impedance.frequency, real_impedance=self.impedance.real_impedance, imaginary_impedance=self.impedance.imaginary_impedance,
                             ax_sci_notation='both', scientific_limit=3, scientific_label_colorbar=False, legend_label=True,
-                            voltage=self.voltage, norm_color=False)
-                # plot.nyquist_fit(ax=ax, frequency=self.frequency, real_impedance=self.real_impedance,
-                #                  imaginary_impedance=self.imaginary_impedance, Z_fit=self.Z_fit, chi=self.chi_val,
+                            voltage=self.voltage, norm_color=True)
+                # plot.nyquist_fit(ax=ax, frequency=self.impedance.frequency, real_impedance=self.impedance.real_impedance,
+                #                  imaginary_impedance=self.impedance.imaginary_impedance, Z_fit=self.Z_fit, chi=self.chi_val,
                 #                  suggested_circuit=self.custom_circuit.circuit,
                 #                  ax_sci_notation="both", scientific_limit=3, scientific_label_colorbar=False, legend_label=True,
                 #                  voltage=self.voltage, norm_color=True)
@@ -138,13 +123,15 @@ class EIS:
         # Save the fitted circuit
         name = utils.assemble_file_name(self.__class__.__name__, "circuit.json")
         self.custom_circuit.save(os.path.join(save_dir, f"{name}"))
+        
+        added_data = {'rc_linKK': self.num_rc_linKK, "eval_fit_linKK": self.eval_fit_linKK}
+        utils.append_to_save_data(directory=save_dir, added_data=added_data, name=name)
         # Save the dataset
-        data = utils.assemble_data_frame(**{"frequency": self.frequency,"real_impedance": self.real_impedance,
-                                        "imaginary_impedance": self.imaginary_impedance,
-                                        "fit_real_impedance": np.real(self.Z_fit), "fit_imag_impedance": np.imag(self.Z_fit),
-                                        "residual_real":self.res_real, "residual_imag":self.res_imag})
+        data = utils.assemble_data_frame(**{"frequency": self.impedance.frequency, "impedance": self.impedance.real_impedance + 1j*self.impedance.imaginary_impedance, 
+                                        "fit_impedance": self.Z_fit, "residual_real":self.res_real, "residual_imag":self.res_imag, 
+                                        "Z_linKK": self.Z_linKK})
         data_name = utils.assemble_file_name(self.__class__.__name__, "data.csv")
-        utils.save_data(save_dir, data, data_name)
+        utils.save_data_as_csv(save_dir, data, data_name)
 
     def perform_all_actions(self, save_dir:str, plots:list):
         self.analyze()
@@ -158,8 +145,16 @@ class EIS:
         if not rmse_error < 100:
             log.warning(f"A validated circuit was not found with having a fit error of {rmse_error}")
 
-    def mottschotcky_analysis(self): pass
+class Mottschotcky(EIS):
+    def __init__(self, impedance, suggested_circuit: str = None, initial_value=None, max_rc_element: int = 20, cut_off: float = 0.85, fit_type: str = 'complex', val_low_freq=True):
+        super().__init__(impedance, suggested_circuit, initial_value, max_rc_element, cut_off, fit_type, val_low_freq)
+
+    def mottschotcky_analysis(self): 
+        EIS.analyze()
+        r = self.suggested_circuit.R_value()
     # TODO
+
+    
     # nyquist
     # fit nyquist
     # bode
