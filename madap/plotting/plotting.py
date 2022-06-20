b79dea8bf1514f8999f27d56cae9fac2a46e7bd1
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-06-20 16:45:03 +0200

    automatic and manual tick generation

--- madap/plotting/plotting.py
+++ madap/plotting/plotting.py
@@ -4,6 +4,7 @@ import matplotlib as mpl
 import numpy as np
 from madap import logger
 from matplotlib.axes import Axes
+import math
 
 log = logger.get_logger("plotting")
 class Plots():
@@ -18,7 +19,8 @@ class Plots():
         plt.rcParams['ytick.direction'] = 'in'
 
     def plot_identity(self, ax, xlabel:str=None, ylabel:str=None, x_lim:list=None, y_lim:list=None, rotation:float=0,
-                      ax_sci_notation=None, scientific_limit=0, log_scale=None):
+                      ax_sci_notation=None, scientific_limit=0, log_scale=None, base10:bool=False, 
+                      step_size_x="auto", step_size_y="auto"):
         # ax_sci_notation (str, optiopnal): whether or not an axis should be written with
         #         scientific notation. It can be 'x', 'y', 'both' or None.
         if xlabel:
@@ -26,11 +28,13 @@ class Plots():
         if ylabel:
             ax.set_ylabel(ylabel, fontsize=9)
         if x_lim:
+            step = (max(x_lim)-min(x_lim))/5 if step_size_x=="auto" else step_size_x
             ax.set_xlim([min(x_lim), max(x_lim)])
-            ax.set_xticks(np.arange(min(x_lim), max(x_lim), max(x_lim)/5))
+            ax.set_xticks(np.arange(min(x_lim), max(x_lim), step))
         if y_lim:
+            step = (max(y_lim)-min(y_lim))/5 if step_size_y=="auto" else step_size_y
             ax.set_ylim([min(y_lim), max(y_lim)])
-            ax.set_yticks(np.arange(min(y_lim), max(y_lim), max(y_lim)/5))
+            ax.set_yticks(np.arange(min(y_lim), max(y_lim), step))
         if rotation:
             ax.xaxis.set_tick_params(rotation=rotation)
         if ax_sci_notation:
@@ -53,7 +57,19 @@ class Plots():
                 cb.formatter.set_powerlimits((scientific_limit, scientific_limit))
                 cb.update_ticks()
 
-
+    def round_hundredth(self, nums):
+        if not nums.all() < 0: 
+            min_num, max_num = int(math.ceil(min(nums) / 100.0)) * 100 - 100, int(math.ceil(max(nums) / 100.0)) * 100
+        else: 
+            min_num, max_num = int(math.ceil(min(nums) / 100.0)) * 100 , int(math.ceil(max(nums) / 100.0)) * 100+ 100
+        return min_num, max_num
+    
+    def round_tenth(self, nums):
+        if not nums.all() < 0:
+            min_num, max_num = round(min(nums), -1) - 10, round(max(nums), -1)
+        else:
+            min_num, max_num = round(min(nums), -1) , round(max(nums), -1) + 10
+        return min_num, max_num
 
     def compose_subplot():
         pass
