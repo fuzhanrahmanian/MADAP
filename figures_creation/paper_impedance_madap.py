339addc2c4720258e953990c220dde5bc3afdd51
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-08-16 23:51:02 +0200

    added scripts for figure creation

---
+++ figures_creation/paper_impedance_madap.py
@@ -0,0 +1,7 @@
+# impedance plots
+# for loop EIS make plot for all, get the cell constant for each
+# from the jsons extract conductivity and and resistance
+# copy the current dataFrame and add predicted conductivity and resistance
+# headers are ;experimentID;electrolyteLabel;PC;EC;EMC;LiPF6;inverseTemperature;temperature;conductivity;resistance;Z';Z'';frequency;electrolyteAmount
+# plot conductivity vs. (EC/PC) colorbar (LiPF6/EMC)
+# add default and get circuits to the dataFrame
