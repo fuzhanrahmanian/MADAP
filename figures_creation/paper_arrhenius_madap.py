ccd0748c91f53b020f716604e4e5199dc114afd8
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-08-17 22:29:08 +0200

    Started activation plot

--- figures_creation/paper_arrhenius_madap.py
+++ figures_creation/paper_arrhenius_madap.py
@@ -12,11 +12,17 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from tqdm import tqdm
 import pandas as pd
+import numpy as np
 from fractions import Fraction
+from tqdm import tqdm
 
 import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.use('Agg')
+import matplotlib.markers as mmarkers
+import matplotlib.patches as mpatches
+from matplotlib.lines import Line2D
+
+#matplotlib.use('Agg')
 matplotlib.rcParams["figure.max_open_warning"] = 1500
 
 from madap.plotting import plotting
@@ -24,6 +30,7 @@ from madap.echem.arrhenius import arrhenius as arr
 from madap.data_acquisition import data_acquisition as da
 
 TRAIN = False
+plotting.Plots()
 save_dir = os.path.join(os.getcwd(), r"electrolyte_figures/arrhenius")
 
 # fit the data if TRAIN is True, otherwise plot the activation according to different PC, EC, EMC, LiPF6 parameters
@@ -54,40 +61,67 @@ if TRAIN:
 
     data.to_csv(os.path.join(os.getcwd(),r"data/CompleteDataWithArr.csv"), sep=";", index=True)
 
-# round((data["EC"] + data["PC"])/ data["EMC"], 1).unique()
-
 
 
 data = pd.read_csv(os.path.join(os.getcwd(),r"data/CompleteDataWithArr.csv"), sep=";")
 del data['Unnamed: 0']
-# plot the predicted activation for each batch of the experiment
-point_size = [5* (i+1) for i,_ in enumerate(data["temperature"].unique())]
-markers = ["o", "^", "D"]
-fraction_ratio = [Fraction(perc) for perc in round((data["EC"] + data["PC"])/ data["EMC"], 1).unique()]
 
-sp = plt.scatter(data["EC"]/data["PC"], data["activation_[mJ/mol]"], \
-            marker = round((data["EC"] + data["PC"])/ data["EMC"], 1),\
-            c = data["LiPF6"], cmap = "viridis", s = point_size, ls = "none")
-            #label = data["temperauter"], alpha = 0.5)
+Ec_PC_ratio = data["EC"]/data["PC"]
+EC_PC_EMC_raio = round((data["EC"] + data["PC"])/ data["EMC"], 1)
 
-# manage multilegend
-f = lambda m,s: plt.scatter([], [], marker = m, s = s, ls = "none")[0]
 
-handles = [f("s", point_size[i]) for i in range(len(point_size))]
-handles += [f(markers[i], "k") for i in range(len(markers))]
+# specify the figure markers
+marker_sign = []
+for i in range(len(data)):
+    if EC_PC_EMC_raio.unique()[0] == EC_PC_EMC_raio.iloc[i]:
+        marker_sign.append("o")
+    elif EC_PC_EMC_raio.unique()[1] == EC_PC_EMC_raio.iloc[i]:
+        marker_sign.append("^")
+    elif EC_PC_EMC_raio.unique()[2] == EC_PC_EMC_raio.iloc[i]:
+        marker_sign.append("D")
 
-labels = point_size, [f"(EC + PC): EMC = {fraction_ratio[0]}",\
-                        f"(EC + PC): EMC = {fraction_ratio[1]}",\
-                        f"(EC + PC): EMC = {fraction_ratio[2]}"]
+# append the EC/PC ratio to the dataFrame
+data = pd.concat([data, pd.DataFrame(data["EC"]/data["PC"], columns= ["EC/PC"])], axis= 1)
+data = pd.concat([data, pd.DataFrame(marker_sign, columns= ["marker"]) ], axis= 1)
+
+# define a list acceptable markers for the plot
+def mscatter(x,y,ax=None, m=None, **kw):
+    if not ax: ax=plt.gca()
+    sc = ax.scatter(x,y,**kw)
+    if (m is not None) and (len(m)==len(x)):
+        paths = []
+        for marker in m:
+            if isinstance(marker, mmarkers.MarkerStyle):
+                marker_obj = marker
+            else:
+                marker_obj = mmarkers.MarkerStyle(marker)
+            path = marker_obj.get_path().transformed(
+                        marker_obj.get_transform())
+            paths.append(path)
+        sc.set_paths(paths)
+    return sc
+
+
+fig, ax = plt.subplots(figsize=(4.9,4))
+scatter = mscatter(x=np.array(data["EC/PC"]) , y=  np.array(data["activation_[mJ/mol]"]),\
+                    c= np.array(data["LiPF6"]), s=9, m=data["marker"], ax=ax)
+
+legend_elements = [Line2D([0], [0], marker='o', color='k', label=f'label=f"(EC + PC): EMC = {EC_PC_EMC_raio.unique()[0]}',
+                          markerfacecolor='k', markersize=15),
+                   Line2D([0], [0], marker='o', color='k', label=f"(EC + PC): EMC = {EC_PC_EMC_raio.unique()[0]}",
+                          markerfacecolor='k', markersize=15),
+                   Line2D([0], [0], marker='o', color='k', label=f'(EC + PC): EMC = {EC_PC_EMC_raio.unique()[0]}',
+                          markerfacecolor='k', markersize=15)]
 
 # add colorbar
-cb = plt.colorbar(sp)
-cb.set_label(label = r"LiPF_6 [gr]")
+cb = plt.colorbar(scatter)
+cb.set_label(label = r"$LiPF_6$ $[g]$")
 
 # add label to the axes
-plt.xlabel("EC/PC [gr/gr")
-plt.ylabel("E [mJ/mol]")
+plt.xlabel(r"$EC/PC$ $[g/g]$")
+plt.ylabel(r"$E$ $[mJ/mol]$")
 
-plt.legend(handles, labels)
-plt.show()
-#plt.savefig(os.path.join(save_dir, "activation_EC_PC_EMC.png"))
+#plt.legend(handles, labels)
+plt.legend(handles=legend_elements)#, loc="best", frameon=False)
+#plt.show()
+plt.savefig(os.path.join(save_dir, "activation_EC_PC_EMC.svg"))
