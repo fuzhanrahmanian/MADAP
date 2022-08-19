e8e91fbc1995a98e7a1f1819d0c4463c4017788b
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-08-20 00:09:45 +0200

    run the madap on main dataframe

--- figures_creation/paper_arrhenius_madap.py
+++ figures_creation/paper_arrhenius_madap.py
@@ -6,22 +6,18 @@
 # headers are ;experimentID;electrolyteLabel;PC;EC;EMC;LiPF6;inverseTemperature;temperature;conductivity;resistance;Z';Z'';frequency;electrolyteAmount
 # plot activation vs. (EC/PC) colorbar (LiPF6) scatter point circle and triangle for different EMC concentrationS
 # add an activation parameter to the dataFrame
-from cmath import exp
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
-from fractions import Fraction
 from tqdm import tqdm
 
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.markers as mmarkers
-import matplotlib.patches as mpatches
 from matplotlib.lines import Line2D
-
 #matplotlib.use('Agg')
 matplotlib.rcParams["figure.max_open_warning"] = 1500
 
@@ -29,77 +25,35 @@ from madap.plotting import plotting
 from madap.echem.arrhenius import arrhenius as arr
 from madap.data_acquisition import data_acquisition as da
 
-TRAIN = False
 plotting.Plots()
 save_dir = os.path.join(os.getcwd(), r"electrolyte_figures/arrhenius")
 
-# fit the data if TRAIN is True, otherwise plot the activation according to different PC, EC, EMC, LiPF6 parameters
-if TRAIN:
+plot_type = ["arrhenius", "arrhenius_fit"]
 
-    plot_type = ["arrhenius", "arrhenius_fit"]
-
-    # load the data
-    data = pd.read_csv(os.path.join(os.getcwd(),r"data/CompleteData.csv"), sep=";")
-    del data['Unnamed: 0']
-
-    for exp_id in tqdm(data["experimentID"].unique()):
-        # get the data for the current experiment
-        temp_exp = data["temperature"][data["experimentID"] == exp_id]
-        cond_exp = data["conductivity"][data["experimentID"] == exp_id]
-
-        # initialize the Arrhenius class
-        Arr = arr.Arrhenius(da.format_data(temp_exp), da.format_data(cond_exp))
-        # analyze the data
-        Arr.perform_all_actions(save_dir, plots = da.format_plots(plot_type), optional_name=exp_id)
-
-        # add fit score, activation constant and activation to the dataFrame
-        data.loc[data["experimentID"] == exp_id, "activation_[mJ/mol]"] = Arr.activation
-        data.loc[data["experimentID"] == exp_id, "activation_constant_[S/cm]"] = Arr.arrhenius_constant
-        data.loc[data["experimentID"] == exp_id, "activation_fit_score"] = Arr.fit_score
-
-        #print(data.head(n=20))
-
-    data.to_csv(os.path.join(os.getcwd(),r"data/CompleteDataWithArr.csv"), sep=";", index=True)
-
-
-
-data = pd.read_csv(os.path.join(os.getcwd(),r"data/CompleteDataWithArr.csv"), sep=";")
+# load the data
+data = pd.read_csv(os.path.join(os.getcwd(),r"data/Dataframe_STRUCTURED_all508.csv"), sep=";")
 del data['Unnamed: 0']
 
-Ec_PC_ratio = data["EC"]/data["PC"]
-EC_PC_EMC_ratio = round((data["EC"] + data["PC"])/ data["EMC"], 1)
+for exp_id in tqdm(data["experimentID"].unique()):
+    # get the data for the current experiment
+    temp_exp = data["temperature [°C]"][data["experimentID"] == exp_id]
+    cond_exp = data["conductivity [S/cm]"][data["experimentID"] == exp_id]
 
+    # initialize the Arrhenius class
+    Arr = arr.Arrhenius(da.format_data(temp_exp), da.format_data(cond_exp))
+    # analyze the data
+    Arr.perform_all_actions(save_dir, plots = da.format_plots(plot_type), optional_name=exp_id)
 
-# specify the figure markers
-marker_sign = []
-for i in range(len(data)):
-    if EC_PC_EMC_ratio.unique()[0] == EC_PC_EMC_ratio.iloc[i]:
-        marker_sign.append("o")
-    elif EC_PC_EMC_ratio.unique()[1] == EC_PC_EMC_ratio.iloc[i]:
-        marker_sign.append("^")
-    elif EC_PC_EMC_ratio.unique()[2] == EC_PC_EMC_ratio.iloc[i]:
-        marker_sign.append("D")
+    # add fit score, activation constant and activation to the dataFrame
+    data.loc[data["experimentID"] == exp_id, "activation_energy [mJ/mol]"] = Arr.activation
+    data.loc[data["experimentID"] == exp_id, "activation_constant [S/cm]"] = Arr.arrhenius_constant
+    data.loc[data["experimentID"] == exp_id, "activation_r2_score"] = Arr.fit_score
+    data.loc[data["experimentID"] == exp_id, "activation_mse"] = Arr.mse_calc
 
-# append the EC/PC ratio to the dataFrame
-data = pd.concat([data, pd.DataFrame(data["EC"]/data["PC"], columns= ["EC/PC"])], axis= 1)
-data = pd.concat([data, pd.DataFrame(marker_sign, columns= ["marker"]) ], axis= 1)
+    #print(data.head(n=20))
+data.to_csv(os.path.join(os.getcwd(),r"data/Dataframe_STRUCTURED_all508_arr.csv"), sep=";", index=True)
 
-# define a list acceptable markers for the plot
-def mscatter(x,y,ax=None, m=None, **kw):
-    if not ax: ax=plt.gca()
-    sc = ax.scatter(x,y,**kw)
-    if (m is not None) and (len(m)==len(x)):
-        paths = []
-        for marker in m:
-            if isinstance(marker, mmarkers.MarkerStyle):
-                marker_obj = marker
-            else:
-                marker_obj = mmarkers.MarkerStyle(marker)
-            path = marker_obj.get_path().transformed(
-                        marker_obj.get_transform())
-            paths.append(path)
-        sc.set_paths(paths)
-    return sc
+data.to_csv(os.path.join(os.getcwd(),r"data/Dataframe_STRUCTURED_all508.csv"), sep=";", index=True, mode='w+')
 
 # plot the activation vs. (EC/PC) colorbar (LiPF6) scatter point circle and triangle for different EMC concentrationS
 fig, ax = plt.subplots(figsize=(4.9,4))
--- figures_creation/paper_impedance_madap.py
+++ figures_creation/paper_impedance_madap.py
@@ -5,3 +5,76 @@
 # headers are ;experimentID;electrolyteLabel;PC;EC;EMC;LiPF6;inverseTemperature;temperature;conductivity;resistance;Z';Z'';frequency;electrolyteAmount
 # plot conductivity vs. (EC/PC) colorbar (LiPF6/EMC)
 # add default and get circuits to the dataFrame
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.markers as mmarkers
+from matplotlib.lines import Line2D
+
+#matplotlib.use('Agg')
+matplotlib.rcParams["figure.max_open_warning"] = 1500
+
+from madap.plotting import plotting
+from madap.echem.e_impedance import e_impedance as imp
+from madap.data_acquisition import data_acquisition as da
+
+
+DEFAULTTRAIN = True
+CUSTOMTRAIN = False
+
+plotting.Plots()
+save_dir = os.path.join(os.getcwd(), r"electrolyte_figures/impedance")
+
+plot_type = ["nyquist" ,"nyquist_fit", "residual", "bode"]
+
+# load the data
+data = pd.read_csv(os.path.join(os.getcwd(),r"data/Dataframe_STRUCTURED_all508.csv"), sep=";")
+del data['Unnamed: 0']
+
+# one time train without a custom circuit and with the default circuit
+if DEFAULTTRAIN:
+    for exp_id in tqdm(data["experimentID"].unique()):
+        for temp in data["temperature [°C]"].unique():
+        # get the data for the current experiment
+            freq_data =eval(data.loc[(data["experimentID"] == exp_id) & (data["temperature [°C]"] == temp), "frequency [Hz]"][0])
+            real_data = eval(data.loc[(data["experimentID"] == exp_id) & (data["temperature [°C]"] == temp), "real impedance Z' [Ohm]"][0])
+            imag_data = eval(data.loc[(data["experimentID"] == exp_id) & (data["temperature [°C]"] == temp), "imaginary impedance Z'' [Ohm]"][0])
+            phase_shift_data = None
+            cell_constant = eval(data.loc[(data["experimentID"] == exp_id) & (data["temperature [°C]"] == temp), "cell constant, standard deviation"][0])[0]
+            # initialize the Impedance class
+            Im = imp.EImpedance(da.format_data(freq_data), da.format_data(real_data), da.format_data(imag_data), phase_shift_data)
+
+            # initialis the EIS procedure
+            Eis  = imp.EIS(Im, voltage=None, suggested_circuit="R0-p(R1,CPE1)",
+                                    initial_value=[800,1e+14,1e-9,0.8], cell_constant = cell_constant)
+            # analyze the data
+            Eis.perform_all_actions(save_dir, plots = da.format_plots(plot_type), optional_name=exp_id)
+
+            # add phaseshift, resistance, Conductivity , Circuit, circuits values, fit score RMSE, errors/confidance
+            # "Fit": true, "Parameters": [1111.447578481177, 481035473447764.56, 7.910124148034937e-08, 0.881950020624159], 
+            # "Confidence": [662.1935795742313, 0.007738940308997105, 9.150118977335364e-09, 0.015475407455327397], 
+            # "rc_linKK": 6, "eval_fit_linKK": 0.7982701895051114, "RMSE_fit_error": 5154.242270867189,
+            # "conductivity [S/cm]": 0.0018894278422646855}
+            #1. phase_shift
+            data.loc[(data["experimentID"] == exp_id) & (data["temperature [°C]"] == temp), "phase_shift \u03c6 [\u00b0]"] = Eis.impedance.phase_shift
+            #2. conductivity
+            data.loc[(data["experimentID"] == exp_id) & (data["temperature [°C]"] == temp), "madap_conductivity_default [S/cm]"] = Eis.conductivity
+            #3. circuit that fit , type of fit, true or false
+            #4. parameters plus theirs errors/confidance
+            #5. RMSE of the fit
+            #6. rc_linKK
+            #7. eval_fit_linKK
+            #8. resistance [Ohm]
+
+if CUSTOMTRAIN: pass
+# one time train with a custom circuit
+        #print(data.head(n=20))
+
+data.to_csv(os.path.join(os.getcwd(),r"data/CompleteDataWithimp.csv"), sep=";", index=True)
