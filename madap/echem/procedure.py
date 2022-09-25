3bc9528ab03f1dff827a66c8cb1e7cd592f15e5d
Fuzhan R <fuzhanrahmanian@gmail.com>
2022-09-25 16:38:39 +0200

    Adjusted linting for e_chem

--- madap/echem/procedure.py
+++ madap/echem/procedure.py
@@ -1,20 +1,43 @@
+"""This module defines the abstract class for the blueprint from all method's behaviours."""
 from abc import ABC, abstractmethod
 
 
 class EChemProcedure(ABC):
+    """ Abstract class for the blueprint from all method's behaviours.
 
+    Args:
+        ABC (class): Abstract Base Class
+    """
     @abstractmethod
     def analyze(self):
-        pass
+        """Abstract method for the analysis of the data.
+        """
 
     @abstractmethod
     def plot(self, save_dir:str, plots:list, optional_name:str):
-        pass
+        """Abstract method for the plotting of the data.
+
+        Args:
+            save_dir (str): The directory where the plots are saved.
+            plots (list): The plots that are saved.
+            optional_name (str): The optional name of the plot.
+        """
 
     @abstractmethod
     def save_data(self, save_dir:str, optional_name:str):
-        pass
+        """Abstract method for the saving of the data.
+
+        Args:
+            save_dir (str): The directory where the data is saved.
+            optional_name (str): The optional name of the data.
+        """
 
     @abstractmethod
     def perform_all_actions(self, save_dir:str, plots:list, optional_name:str):
-        pass
+        """Abstract method for the performing of all actions.
+
+        Args:
+            save_dir (str): The directory where the data is saved.
+            plots (list): The plots that are saved.
+            optional_name (str): The optional name of the data.
+        """
