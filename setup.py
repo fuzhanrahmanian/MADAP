333bc116b8b1c35cccff6650894caf3b4336209b
fuzhanr <fuzhanrahmanian@gmail.com>
2022-10-03 00:21:21 +0200

    Updated Documentation and version

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fuzhanrahmanian/MADAP',
-    version='0.8.0',
+    version='0.9.0',
     zip_safe=False,
 )
