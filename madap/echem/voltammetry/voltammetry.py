7bcaa3e31ceff4978f52d251d544f1d856e70409
fuzhanrahmanian <fuzhanrahmanian@gmail.com>
2023-11-24 00:47:37 +0100

    Initialize the voltammetry class *  unified the units for both current and time * initializing voltammetry params

--- madap/echem/voltammetry/voltammetry.py
+++ madap/echem/voltammetry/voltammetry.py
@@ -1,22 +1,47 @@
-import os
-from attrs import define, field
-from attrs.setters import frozen
 from madap.logger import logger
 from madap.echem.procedure import EChemProcedure
 
 
 log = logger.get_logger("voltammetry")
 
-@define
 class Voltammetry(EChemProcedure):
-    voltage = field(default=0)
-    current = field(default=0)
-    time = field(default=0)
+    def __init__(self, voltage, current, time,
+                 measured_current_units:str,
+                 measured_time_units:str,
+                 number_of_electrons:str) -> None:
+        self.voltage = voltage
+        self.current = current
+        self.time = time
+        self.measured_current_unit = measured_current_units
+        self.measured_time_unitis = measured_time_units
+        self.number_of_electrons = int(number_of_electrons)
+        self.convert_current()
+        self.convert_time()
 
-    def _calculate_capacity(self):
-        # TODO di/dt
-        pass
+    def convert_current(self):
+        """Convert the current to A indipendently from the unit of measure
+        """
+        if self.measured_current_unit == "uA":
+            self.current = self.current * 1e-6
+        elif self.measured_current_unit == "mA":
+            self.current = self.current * 1e-3
+        elif self.measured_current_unit == "A":
+            self.current = self.current * 1e0
+        else:
+            log.error(f"Current unit not supported. Supported units are: uA, mA, A")
+            raise ValueError("Current unit not supported")
 
-    def _calculate_columbic_efficiency(self):
-        # TODO
-        pass
+    def convert_time(self):
+        """Convert the time to s indipendently from the unit of measure
+        """
+        if self.measured_time_unitis == "s":
+            self.time = self.time * 1e0
+        elif self.measured_time_unitis == "min":
+            self.time = self.time * 60
+        elif self.measured_time_unitis == "h":
+            self.time = self.time * 3600
+        elif self.measured_time_unitis == "ms":
+            self.time = self.time * 1e-3
+        else:
+            log.error(f"Time unit not supported. Supported units are: s, min, h")
+            raise ValueError("Time unit not supported")
